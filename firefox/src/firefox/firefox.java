93067f7d16e6247434d00acdf8625bdddec9114f
sasirekha_a <sasirekha_a@C1128.solartis.madurai.com>
2016-11-10 12:45:29 +0530

    commit1

---
+++ firefox/src/firefox/firefox.java
@@ -0,0 +1,13 @@
+package firefox;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class firefox {
+	public static void main(String args[])
+	{
+	FirefoxDriver dr1=new FirefoxDriver();
+	dr1.get("http://google.com");
+	System.out.println("completed git");
+	}
+
+}
