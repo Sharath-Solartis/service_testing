58ebc3d5c6c0cca65966f6e45849c019425cfbff
Sharath-Solartis <sharath_n@solartis.net>
2017-02-15 15:49:43 +0530

    Comment Added
    Checking GIthub Tool

--- Star_Search_Rescue/src/main/java/Rating_API/Star_Search_Rescue/app_db.java
+++ Star_Search_Rescue/src/main/java/Rating_API/Star_Search_Rescue/app_db.java
@@ -27,7 +27,7 @@ public class app_db
 		database_operation.config = new properties_handle
 				("Q:/Automation Team/1 Projects/06 Star Search_rescue/Search_n_Rescue/program_doc/Release_1/Rating/configuration_file/config_json.properties");
 		//Comment Here
-		
+		//Comment Here
 		database_operation.conn_setup();
     	System.setProperty("jsse.enableSNIExtension", "false");
 		
